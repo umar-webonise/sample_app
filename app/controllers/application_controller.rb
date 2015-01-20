bd565978d1b7c1934971ef472010028ee34aa875
Umar Siddiqui <umar.s@weboniselab.com>
2015-01-20 10:21:59 +0530

    changed default page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  render text: '<h1>Hello World!!</h1>'
+  def hello
+    render text: '<h1>Sample App!!</h1>'
+  end
 end
