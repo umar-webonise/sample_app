9f2f05f3d828415cb4b37f591a81f9cbab5429e3
Umar Siddiqui <umar.s@weboniselab.com>
2015-01-20 18:55:41 +0530

    added users controller and routes for signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   get 'static_pages/help'
 
   get 'about'   => 'static_pages#about'
@@ -6,6 +8,8 @@ Rails.application.routes.draw do
   get 'contact' => 'static_pages#contact'
 
   get 'help'   => 'static_pages#help'
+
+  get 'signup'   => 'users#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
