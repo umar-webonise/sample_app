692c193bfddfbd9523324865f014b48af7074de8
Umar Siddiqui <umar.s@weboniselab.com>
2015-01-20 18:49:52 +0530

    Implemented all static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   get 'static_pages/help'
 
-  get 'static_pages/about'
+  get 'about'   => 'static_pages#about'
+
+  get 'contact' => 'static_pages#contact'
+
+  get 'help'   => 'static_pages#help'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
