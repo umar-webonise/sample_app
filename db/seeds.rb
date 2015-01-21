6f79c15d4cd0e0007f44b58727fd27623d1aeca6
Umar Siddiqui <umar.s@weboniselab.com>
2015-01-22 03:56:13 +0530

    modified seed code

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create!(name:  "Umar Siddiqui",
-             email: "umar.s@weboniselab.com",
-             password:              "foobar",
-             password_confirmation: "foobar",
-             admin: true)
+User.create!(name:  'Umar Siddiqui',
+             email: 'umar.s@weboniselab.com',
+             password:              'foobar',
+             password_confirmation: 'foobar',
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
-  password = "password"
+  password = 'password'
   User.create!(name:  name,
                email: email,
                password:              password,
                password_confirmation: password,
-               admin: true)
+               activated: true,
+               activated_at: Time.zone.now)
 end
