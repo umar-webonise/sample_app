cde1747c3beeeb2b921bb13d50cf24b6e1356c27
Umar Siddiqui <umar.s@weboniselab.com>
2015-01-22 19:44:40 +0530

    Add user following

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @micropost = current_user.microposts.build
-      @feed_items = current_user.microposts
+      @feed_items = current_user.feed
     end
   end
 
