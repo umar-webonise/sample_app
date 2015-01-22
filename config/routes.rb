2c68532e9e25abc568651ea6c24fd9e81dd44423
Umar Siddiqui <umar.s@weboniselab.com>
2015-01-22 16:33:12 +0530

    Display follwers and following provided by seeddata

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 
   resources :account_activations, only: [:edit]
 
   resources :password_resets,     only: [:new, :create, :edit, :update]
 
   resources :microposts,          only: [:create, :destroy]
+
+  resources :relationships,       only: [:create, :destroy]
 
   get 'about'   => 'static_pages#about'
 
