09d99c05249572dde9cb6bba00d0dd55b9c17ae3
Umar Siddiqui <umar.s@weboniselab.com>
2015-01-22 09:16:46 +0530

    addded restful apis for microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts
   end
 
   def new
@@ -53,15 +54,6 @@ class UsersController < ApplicationController
   end
 
   # Before filters
-
-  # Confirms a logged-in user.
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = 'Please log in.'
-      redirect_to login_url
-    end
-  end
 
   # Confirms the correct user.
   def correct_user
